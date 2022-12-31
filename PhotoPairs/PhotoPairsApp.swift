e6269734551c952e5b33ff9daeef8a9a9399f105
Mijo Gracanin <mijo.gracanin@gmail.com>
2022-12-31 17:06:59 +0100

    Initial Commit

---
+++ PhotoPairs/PhotoPairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPairsApp.swift
+//  PhotoPairs
+//
+//  Created by Mijo Gracanin on 31.12.2022..
+//
+
+import SwiftUI
+
+@main
+struct PhotoPairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
